5a6934ec1e142b72535060b78fdc99d85f122dd8
Hisham Mubarak <parengalhisham@gmail.com>
2019-04-17 21:51:13 +0530

    Console Change

--- src/resources/course/course.controller.js
+++ src/resources/course/course.controller.js
@@ -4,20 +4,20 @@ import { Types } from 'mongoose'
 export const getCourse = async (req, res) => {
     const course = await Course.aggregate(
         [
-            { $match: { _id:Types.ObjectId(req.params.id) }},
-            { 
+            { $match: { _id: Types.ObjectId(req.params.id) } },
+            {
                 $lookup: {
-                    from:"subjects",
-                    localField:"_id",
-                    foreignField:"course",
-                    as:"subjects"
+                    from: "subjects",
+                    localField: "_id",
+                    foreignField: "course",
+                    as: "subjects"
                 }
             }
         ]
     )
 
-    if(!course) {
-        return res.status(400).json({ message:"Course not found" })
+    if (!course) {
+        return res.status(400).json({ message: "Courses not found" })
     }
 
     return res.status(200).json(course)
@@ -25,8 +25,8 @@ export const getCourse = async (req, res) => {
 }
 
 export const create = async (req, res) => {
-    if(!req.body.department && !req.body.name) {
-        return res.status(400).send({ message:"Course Name required" })
+    if (!req.body.department && !req.body.name) {
+        return res.status(400).send({ message: "Course Name required" })
     }
 
     try {
