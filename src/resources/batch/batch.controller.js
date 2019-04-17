24e081d06a7153869807d4d8553ca9f97aa56fe5
Hisham Mubarak <parengalhisham@gmail.com>
2019-04-17 21:54:26 +0530

    Console gix

--- src/resources/batch/batch.controller.js
+++ src/resources/batch/batch.controller.js
@@ -6,48 +6,48 @@ import { Types } from 'mongoose'
 export const addBatch = createOne(Batch)
 
 export const getOneBatch = async (req, res) => {
-    if(!req.params.id) {
-        return res.status(400).json({ message:"batchId required" })
+    if (!req.params.id) {
+        return res.status(400).json({ message: "batchId required" })
     }
 
     try {
-    
+
         const batchData = await Batch.aggregate([
-            { $match: { _id:Types.ObjectId(req.params.id) }},
+            { $match: { _id: Types.ObjectId(req.params.id) } },
             {
                 $lookup: {
                     from: "courses",
-                    localField:"course",
-                    foreignField:"_id",
-                    as:"course"
+                    localField: "course",
+                    foreignField: "_id",
+                    as: "course"
                 }
             },
             {
                 $lookup: {
                     from: "departments",
-                    localField:"department",
-                    foreignField:"_id",
-                    as:"department"
+                    localField: "department",
+                    foreignField: "_id",
+                    as: "department"
                 }
             }
         ])
-    
-        if(!batchData) {
-            return res.status(400).json({ message:"No batch foudn with provided batchId" })
+
+        if (!batchData) {
+            return res.status(400).json({ message: "No batch found with provided batchId" })
         }
 
         const students = await Student.find({ batch: Types.ObjectId(req.params.id) })
-        
+
         return res.status(200).json({ batchData, students })
 
     } catch (e) {
-        return res.status(500).json({ message:"DB Error aggreagating batch data" })
+        return res.status(500).json({ message: "DB Error aggreagating batch data" })
     }
-    
+
 }
 
 export const editBatch = updateOne(Batch)
 
 export const deleteBatch = removeOne(Batch)
 
-export const getManyBatch =  getMany(Batch)
+export const getManyBatch = getMany(Batch)
