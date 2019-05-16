488a1636d2309ca0814fd13e934e1e4987adba10
Hisham Mubarak <parengalhisham@gmail.com>
2019-05-16 21:40:41 +0530

    Fetch department teachers only for HoD

--- src/resources/teacher/teacher.controller.js
+++ src/resources/teacher/teacher.controller.js
@@ -2,8 +2,14 @@ import { crudControllers } from '../../utils/crud'
 import { Teacher } from './teacher.model'
 
 export const getTeachers = async (req, res) => {
+    let matchObject = {}
+    
+    if(req.query.roleId === "2") {
+        matchObject.department = req.query.departmentId
+    }
+    
     try {
-        const Teachers = await Teacher.find({}).populate('department')
+        const Teachers = await Teacher.find(matchObject).populate('department')
         return res.status(200).json(Teachers)
     } catch (e) {
         return res.status(500).json({ message: "DB Error getting teachers" })
